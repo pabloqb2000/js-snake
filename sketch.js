24d7370661453e8468c77985b70bfcbf7b1134d6
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-05-23 20:46:25 +0200

    Most of the implementation done

--- sketch.js
+++ sketch.js
@@ -1,54 +1,168 @@
-let sld;
-let btn;
-let tggl;
-let cPicker;
+let nPlayers;
+let nCells, cellSize;
+let players = [], foods = [];
+let foodChance, superFoodChance, superFoodPower;
+let foodColor, superFoodColor;
+let p1Color, p2Color;
+let startLen, record;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(32);
 
-	// Create UI elements
-	sld = new Slider(start=0, end=255, value=32, 0, 0, width/12, height/60, null, "Background");
-	btn = new Button(x=0, y=0, width/12, height/30, "Reset", resetValue);
-	
-	tggl = new ToggleButton(0,0,width/12,height/30,"Discrete", discretice);
-	cPicker = new ColorPicker(0,0, width/12, height/60);
-
-	// Start UI
-	UI.tableWidth = 1;
-	UI.tableHeight = 100;
-	UI.distrubute();
+	// Parameters
+	nPlayers = 0;
+	nCells = 16; // should be even
+	cellSize = height/20;
+	p1Color = color(227, 103, 86);
+	p2Color = color(86, 210, 227);
+	startLen = 12;
+	record = startLen;
+	foodChance = 0.1;
+	superFoodChance = 0.01;
+	superFoodPower = 5;
+	foodColor = color(105, 227, 86);
+	superFoodColor = color(227, 226, 86);
+	frameRate(1);
 }
 
 function draw() {
-	// Draw UI
-	background(sld.value);
-	UI.update();
-	UI.draw();
+	background(32);
+	translate(width/2, height/2);
+	if(nPlayers == 0) {
+		drawStartMenu();
+	} else {
+		gameLoop();
+	}
+}
 
+function drawStartMenu() {
+	nPlayers = 2;
+	players.push(new Snake(0));
+	players.push(new Snake(1));
+}
+
+function gameLoop() {
+	// Draw map boundaries
+	strokeWeight(1);
+	stroke(200);
+	noFill();
+	rect(-nCells/2 * cellSize, -nCells/2 * cellSize, nCells * cellSize, nCells * cellSize);
+
+	// Update players
+	for(let p of players) {
+		p.update();
+	}
+
+	// Draw players
+	noStroke();
+	for(let p of players) {
+		p.draw();
+	}
+
+	// Spawn and draw food
+	spawnFood();
+	drawFood();
+
+	// Draw points
+	textSize(cellSize);
+	fill(p1Color);
+	text("Player1", nCells/2 * cellSize + cellSize, -nCells/2 * cellSize + cellSize);
+	text(players[0].getPoints(), nCells/2 * cellSize + cellSize, -nCells/2 * cellSize + 2*cellSize + 10);
 	
-	translate(width/2 + width/12, height/2);
-	scale(1,-1);
+	fill(200);
+	text("Record", nCells/2 * cellSize + cellSize, -cellSize/2 - 5);
+	text(record, nCells/2 * cellSize + cellSize, cellSize/2 + 5);
+
+	if(nPlayers == 2) {
+		fill(p2Color);
+		text("Player2", nCells/2 * cellSize + cellSize, nCells/2 * cellSize - cellSize - 10);
+		text(players[1].getPoints(), nCells/2 * cellSize + cellSize, nCells/2 * cellSize);
+	}
 }
 
-function resetValue() {
-	sld.value = 32;
+/**
+ * Randomly spawns food in unOcupied cells
+ */
+function spawnFood() {
+	if(random() < foodChance) {
+		let p = createVector(floor(random(-nCells/2, nCells/2)), floor(random(-nCells/2, nCells/2)));
+		while(isOcupied(p)) {
+			p = createVector(floor(random(-nCells/2, nCells/2)), floor(random(-nCells/2, nCells/2)));
+		}
+		foods.push([p,1]);
+	} else if(random() < superFoodChance) {
+		let p = createVector(floor(random(-nCells/2, nCells/2)), floor(random(-nCells/2, nCells/2)));
+		while(isOcupied(p)) {
+			p = createVector(floor(random(-nCells/2, nCells/2)), floor(random(-nCells/2, nCells/2)));
+		}
+		foods.push([p,superFoodPower]);
+	}
 }
 
-function discretice() {
-	sld.step = (sld.step == null ? 20 : null);
+/**
+ * Draws all the food in the map
+ */
+function drawFood() {
+	noStroke();
+	for(let f of foods) {
+		fill(f[1] > 1 ? superFoodColor : foodColor);
+		let r = f[1] > 1 ? cellSize/2 : cellSize/3;
+		ellipse(f[0].x*cellSize + cellSize/2, f[0].y*cellSize + cellSize/2, r, r);
+	}
 }
 
-function mouseDragged() {
-	UI.mouseDragged();
+/**
+ * @param p 
+ * 
+ * @return true if position p is occupied by a player or other food
+ */
+function isOcupied(p) {
+	for(let ply of players)
+		for(let c of ply.cells)
+			if(c.x == p.x && c.y == p.y)
+				return true;
+	for(let f of foods)
+		if(f[0].x == p.x && f[0].y == p.y)
+			return true;
+	return false;
 }
 
 function mousePressed() {
 	UI.mouseClicked();
 }
 
-// function keyPressed() {
-//   if(keyCode === 83){
-//
-//   }
-// }
+function keyPressed() {
+	switch(keyCode){
+		case 87: // W
+			players[0].changeDir(0);
+			break;
+		case 68: // D
+			players[0].changeDir(1);
+			break;
+		case 83: // S
+			players[0].changeDir(2);
+			break;
+		case 65: // A
+			players[0].changeDir(3);
+			break;
+		
+	}
+	if(nPlayers == 2) {
+		switch(keyCode){
+			case 73: // I
+				players[1].changeDir(0);
+				break;
+			case 76: // L
+				players[1].changeDir(1);
+				break;
+			case 75: // K
+				players[1].changeDir(2);
+				break;
+			case 74: // J
+				players[1].changeDir(3);
+				break;
+			
+		}
+	}
+}
---
+++ snake.js
@@ -0,0 +1,132 @@
+class Snake {
+    /**
+     * 
+     * @param n Number of the snake (0 for player 1 and 1 for player 2)
+     */
+    constructor(n) {
+        this.n = n;
+        this.cells = [];
+        this.growing = 0;
+        if(n == 0) {
+            for(let i = -nCells/2; i < -nCells/2 + startLen; i++) {
+                this.cells.push(createVector(i,-nCells/2));
+            }
+            this.dir = 1;
+        } else {
+            for(let i = nCells/2 - 1; i >= nCells/2 - startLen; i--) {
+                this.cells.push(createVector(i,nCells/2 - 1));
+            }
+            this.dir = 3;
+        }
+    }
+
+    /**
+     * Move the snake
+     */
+    update() {
+        // Add new position
+        let h = this.head();
+        switch(this.dir) {
+            case 0:
+                this.cells.push(createVector(h.x, this.mapMod(h.y-1)));
+                break;
+            case 1:
+                this.cells.push(createVector(this.mapMod(h.x+1), h.y));
+                break;
+            case 2:
+                this.cells.push(createVector(h.x, this.mapMod(h.y+1)));
+                break;
+            case 3:
+                this.cells.push(createVector(this.mapMod(h.x-1), h.y));
+                break;
+        }
+
+        // If snake is not growing remove last cell
+        if(this.growing == 0) {
+            this.cells.splice(0,1);
+        } else {
+            this.growing--;
+        }
+
+        // Eat in the new cell
+        let nh = this.head(); // new head
+            // Eat food
+        for(let i = 0; i < foods.length; i++){
+            if(foods[i][0].x == nh.x && foods[i][0].y == nh.y) {
+                this.growing += foods[i][1];
+                foods.splice(i,1);
+                break;
+            }
+        }
+            // Eat it self
+        for(let i = this.cells.length - 2; i >= 0; i--){
+            if(this.cells[i].x == nh.x && this.cells[i].y == nh.y){
+                this.cells.splice(0,i+1);
+            }
+        }
+        
+
+        this.changed = false;
+    }
+
+    /**
+     * If the given position if out of the map 
+     * returns a new position on the other side of the map
+     * 
+     * @param p Position to calculate
+     */
+    mapMod(p) {
+        if(p >= -nCells/2 && p < nCells/2) {
+            return p;
+        }else if(p <= -nCells/2) {
+            return p + nCells;
+        } else {
+            return p - nCells;
+        }
+    }
+
+    /**
+     * Draw this snake
+     */
+    draw() {
+        // Draw body
+        fill(this.n == 0 ? p1Color : p2Color);
+        noStroke();
+        for(let v of this.cells) {
+            rect(v.x*cellSize + 1, v.y*cellSize + 1, cellSize - 2, cellSize - 2, cellSize/12);
+        }
+
+        // Draw eyes
+        stroke(32);
+        strokeWeight(4);
+        let v = this.head();
+        point(v.x*cellSize + cellSize/3, v.y*cellSize + cellSize/3);
+        point(v.x*cellSize + 2*cellSize/3, v.y*cellSize + cellSize/3);
+    }
+
+    /**
+     * Changes the direction to the given one if it's a legal move
+     * 
+     * @param d 
+     */
+    changeDir(d) {
+        if((d+2) % 4 != this.dir && !this.changed){
+            this.dir = d;
+            this.changed = true;
+        }
+    }
+
+    /**
+     * @return The position of the head
+     */
+    head() {
+        return this.cells[this.cells.length - 1];
+    }
+
+    /**
+     * @return the number of points this player has
+     */
+    getPoints() {
+        return this.cells.length;
+    }
+}
